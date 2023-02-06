91603383f6b9b33bf7dbd21c41099eb4ffd3f348
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-02-06 19:06:13 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/br/dev/tsantana/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tsantana.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
