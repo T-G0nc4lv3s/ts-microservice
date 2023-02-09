ddd513897af7b04b32b8ccba900418a37e257cdd
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-02-09 15:06:06 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/br/dev/tsantana/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.tsantana.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
